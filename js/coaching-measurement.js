9c5e0a426d6eebb589dd44f3dd528aa83bd6006c
Breechay <briceikouebe@gmail.com>
2026-09-20 15:18:40 -0400

    Align funnel tracking with four-step intake

--- js/coaching-measurement.js
+++ js/coaching-measurement.js
@@ -93,7 +93,7 @@
     if (privateVisit()) return;
     var fields = { form_id: 'coaching_inquiry', funnel_version: version };
     params = params || {};
-    if (Number.isInteger(params.step_number) && params.step_number >= 1 && params.step_number <= 5) fields.step_number = params.step_number;
+    if (Number.isInteger(params.step_number) && params.step_number >= 1 && params.step_number <= 4) fields.step_number = params.step_number;
     if (['hero', 'header', 'coaching', 'footer', 'other'].indexOf(params.cta_location) !== -1) fields.cta_location = params.cta_location;
     if (name === 'generate_lead') fields.method = 'coaching_inquiry';
     if (qa) {
@@ -179,7 +179,7 @@
     var review = d.querySelector('#p-read.on');
     var question = d.querySelector('#p-ask.on .q.on');
     var step = question ? Number(question.getAttribute('data-q')) + 1 : 0;
-    if (started && previousStep && ((step === previousStep + 1) || (review && previousStep === 5))) {
+    if (started && previousStep && ((step === previousStep + 1) || (review && previousStep === 4))) {
       once('coaching_step_complete', { step_number: previousStep }, 'complete-' + previousStep);
     }
     if (step) previousStep = step;
